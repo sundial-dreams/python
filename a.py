7ede00fb5c4060d087b65aab324fcf11a23e12ba
sundial-dreams <2031163243@qq.com>
2018-10-29 20:46:57 +0800

    dpf commit

--- a.py
+++ a.py
@@ -15,17 +15,6 @@ event = Event()
 if __name__  == "__main__":
     event.on("data",lambda data:print(data))
     event.emit("data",{"name":"dpf","age":18})
-    def G(n:int):
-    a = [1]
-    b = [1,1]
-    while n:
-        done = yield a
-        if done: return
-        a = b
-        b = [1 if i==0 or i==len(a)-1 else a[i-1] + a[i] for i in range(len(a)+1)]
-
-    for x in G(10):
-        print(*x)
 
 
 
