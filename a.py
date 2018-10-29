82c810edb28cad291afd47130bf01aa5041955c0
sundial-dreams <2031163243@qq.com>
2018-10-29 20:36:28 +0800

    it is dpf commit

--- a.py
+++ a.py
@@ -1,3 +1,23 @@
+class Event(object):
+    def __init__(self):
+        self.pool = {}
+    def on(event, callback):
+        if self.pool[event] is None:self.pool[event] = []
+        self.pool[event].append(callback)
+        return self
+    def emit(event,*args,**keywords):
+        if self.pool[event]:
+            for fn in self.pool[event]:
+                fn(*args,**keywords)
+
+event = Event()
+
 if __name__  == "__main__":
-    print("hello world")
-    print("hello world")
+    event.on("data",lambda data:print(data))
+    event.emit("data",{"name":"dpf","age":18})
+
+
+
+
+
+
