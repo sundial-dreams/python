70bca28190367984440bb43a1761b029653484e7
sundial-dreams <2031163243@qq.com>
2018-10-29 20:45:05 +0800

    dpf commit

--- a.py
+++ a.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Event(object):
     def __init__(self):
         self.pool = {}
@@ -16,9 +15,7 @@ event = Event()
 if __name__  == "__main__":
     event.on("data",lambda data:print(data))
     event.emit("data",{"name":"dpf","age":18})
-=======
-import types
-def G(n:int):
+    def G(n:int):
     a = [1]
     b = [1,1]
     while n:
@@ -27,25 +24,8 @@ def G(n:int):
         a = b
         b = [1 if i==0 or i==len(a)-1 else a[i-1] + a[i] for i in range(len(a)+1)]
 
-
-if __name__  == "__main__":
     for x in G(10):
         print(*x)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> dev
 
 
 
