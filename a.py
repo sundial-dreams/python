ed006f730294ecbea0242cae78924e50f7c6a7b0
sundial-dreams <2031163243@qq.com>
2018-10-29 20:30:49 +0800

    dpf commit

--- a.py
+++ a.py
@@ -1,3 +1,34 @@
+import types
+def G(n:int):
+    a = [1]
+    b = [1,1]
+    while n:
+        done = yield a
+        if done: return
+        a = b
+        b = [1 if i==0 or i==len(a)-1 else a[i-1] + a[i] for i in range(len(a)+1)]
+
+
 if __name__  == "__main__":
-    print("hello world")
-    print("hello world")
+    for x in G(10):
+        print(*x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
